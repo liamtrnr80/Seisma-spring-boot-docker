b3b152b63472f11cccd74f01c744685994b6bbfa
liamtrnr80 <liamtrnr80@gmail.com>
2021-08-03 03:55:01 +1000

    Update Application.java

--- src/main/java/com/example/seisma/Application.java
+++ src/main/java/com/example/seisma/Application.java
@@ -19,34 +19,34 @@ import java.util.ArrayList;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Override
-	public void run(String[] args) throws IOException {
+    @Override
+    public void run(String[] args) throws IOException {
 
-		ArrayList<Payslip> payslips = new ArrayList<>();
+        ArrayList<Payslip> payslips = new ArrayList<>();
 
-		ObjectMapper obj = new ObjectMapper();
+        ObjectMapper obj = new ObjectMapper();
 
-		JsonNode nodes = obj.readTree(new File("src/main/resources/json/employee.json"));
+        JsonNode nodes = obj.readTree(new File("src/main/resources/json/employee.json"));
 
-		for(JsonNode node : nodes) {
-			Employee emp = new Employee(node.path("firstName").asText(), node.path("lastName").asText(), node.path("annualSalary").asInt(), node.path("paymentMonth").asInt(), node.path("superRate").asDouble());
-			Payslip pay = new Payslip(emp);
+        for (JsonNode node : nodes) {
+            Employee emp = new Employee(node.path("firstName").asText(), node.path("lastName").asText(), node.path("annualSalary").asInt(), node.path("paymentMonth").asInt(), node.path("superRate").asDouble());
+            Payslip pay = new Payslip(emp);
 
-			payslips.add(pay);
-		}
+            payslips.add(pay);
+        }
 //
 //		for(Payslip payslip : payslips) {
 //			System.out.println(payslip);
 //		}
 
-		ObjectWriter writer = obj.writer(new DefaultPrettyPrinter());
+        ObjectWriter writer = obj.writer(new DefaultPrettyPrinter());
 
-		writer.writeValue(Paths.get("src/main/resources/json/results2.json").toFile(), payslips);
+        writer.writeValue(Paths.get("src/main/resources/json/results2.json").toFile(), payslips);
 
-	}
+    }
 
 }
