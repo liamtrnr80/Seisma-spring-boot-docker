113322daaf4b312cc06096b092b71b409e5e874b
liamtrnr80 <liamtrnr80@gmail.com>
2021-08-07 21:51:24 +1000

    Update TaxBracket.java

--- src/main/java/com/example/seisma/model/TaxBracket.java
+++ src/main/java/com/example/seisma/model/TaxBracket.java
@@ -2,8 +2,12 @@ package com.example.seisma.model;
 
 public class TaxBracket {
 
-    private int to;
-    private int from;
+    private int toBracket;
+    private int fromBracket;
+    private double percent;
+    private int tax;
 
-    
+    public TaxBracket() {
+
+    }
 }
